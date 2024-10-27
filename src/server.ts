9905fecd5dc515aeafab50793d5d41eb58717431
Mohammad Nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-10-27 10:35:08 +0300

    Set Up the Signaling Server in TypeScript.

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,11 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-// Start HTTP server
 const server = http.createServer(app);
 server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-// Initialize WebSocket server
 const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws: WebSocket) => {
